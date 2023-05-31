11a69e828961af987b8aea6486f0612581983fd8
lyj9186 <lyj9186@gmail.com>
2023-05-31 10:29:36 +0900

    apply material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import { withStyles } from '@mui/material/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maginTop: theme.spacing.unit * 3,
+    overflow: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -35,13 +48,27 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return(
-      <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
         {customers.map(c => {  return  ( <Customer key={c.id}  id={c.id} img={c.img} name={c.name} brithday={c.brithday} job={c.job}/>  ) })  }
-      </div>
+          </TableBody>
+        </Table>
+      </Paper>
           );
     }
 
 }
 
-export default App;
+export default withStyles(styles)(App);
